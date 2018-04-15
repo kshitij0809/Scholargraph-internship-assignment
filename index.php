a44e68218f540f8a1a913656341d8dac9cea17bb
Kshitij Pratap Singh Bhadauria <kshitijpratap0809@gmail.com>
2018-04-15 10:25:33 +0530

    sign up query added

--- index.php
+++ index.php
@@ -2,7 +2,69 @@
 
 	include("connect.php");
 	include("functions.php");
-	?>
+	print_r($_SESSION);
+	if(logged_in())
+	{
+		header("location:profile.php");
+		exit();
+	}
+	
+	$error = "";
+
+	if(isset($_POST['submit']))
+	{
+		print_r($_POST);
+		$firstName = mysqli_real_escape_string($con, $_POST['fname']);
+	    $email = mysqli_real_escape_string($con, $_POST['email']);
+	    $password = $_POST['password'];
+	    $passwordConfirm = $_POST['passwordConfirm'];
+	    
+
+      
+		
+		
+		if(strlen($firstName) < 3)
+		{
+			$error = "First name is too short";
+		}
+		
+		
+		else if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+		{
+			$error = "Please enter valid email address";
+		}
+		else if(email_exists($email, $con))
+		{
+			$error = "Someone is already registered with this email";
+		}
+		else if(strlen($password) < 8)
+		{
+			$error = "Password must be greater than 8 characters";
+		}
+		else if($password !== $passwordConfirm)
+		{
+			$error = "Password does not match";
+		}
+		
+		else
+		{	
+				$password = password_hash($password, PASSWORD_DEFAULT);
+				
+				
+					$insertQuery = "INSERT INTO `users`(`firstName`, `email`, `password`) VALUES ('$firstName','$email','$password')";
+
+
+					if(mysqli_query($con, $insertQuery))
+					{
+						
+							$error = "You are successfully registered";					
+				
+				    }
+		}
+							
+	}
+
+?>
 
 
 
@@ -63,5 +125,3 @@
 
 </html>
 
-<!--<form method="POST" action="index.php" enctype="multipart/form-data">The enctype is needed to upload files or images-->
-<!--<label>First Name:</label>  Label tag is new in html5-->
